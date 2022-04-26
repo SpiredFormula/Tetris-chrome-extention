a744ae7f47600ba67ea2baf6488062a021e3e4aa
SpiredFormula <96860840+SpiredFormula@users.noreply.github.com>
2022-04-26 21:49:19 +1000

    render got an update
    all of the rotations for the blocks are almost done!

--- index.js
+++ index.js
@@ -42,39 +42,184 @@ class Canvas{
     }
 
 
-    Render = (block, x, y) =>  {
-        console.log(block,x,y)
-        // if (this.nextblock === 'line'){
-        //     c.fillStyle = "lightblue"
-        //     c.fillRect(x+31+31, y, 30, 30)
-        // }
-        // else if (this.nextblock === 'box'){
-            
-        // }
-        // else if (this.nextblock === 'L1'){
-            
-        // }
-        // else if (this.nextblock === 'L2'){
-            
-        // }
-        // else if (this.nextblock === 'T'){
-            
-        // }
-        // else if (this.nextblock === 'Z1'){
-            
-        // }
-        // else if (this.nextblock === 'Z2'){
-            
-        // }
+    Render = (block, x, y, orientation) =>  {
+        var c = this.canvas.getContext('2d')
+        var check = 0
+        var tempY = y
+        var tempX = x
+        let orgX = x
+        let orgY = y
+
+        //render functions
+        let AddY = (numofrun, remember) => {
+            while(check != numofrun){
+                tempY = tempY+31
+                if(remember === true){
+                    y = tempY
+                }
+                c.fillRect(x,tempY,30,30)
+                tempY = y
+                check++
+            }
+            check = 0
+        }
+        let AddX = (numofrun, remember) => {
+            while(check != numofrun){
+                tempX = tempX+31
+                if(remember === true){
+                    x = tempX
+                }
+                c.fillRect(tempX,y,30,30)
+                check++
+            }
+            tempX = x
+            check = 0
+        }
+        let MinusX = (numofrun, remember) => {
+            while(check != numofrun){
+                tempX = tempX-31
+                if(remember === true){
+                    x = tempX
+                }
+                c.fillRect(tempX,y,30,30)
+                check++
+            }
+            tempX = x
+            check = 0
+        }
+        let MinusY = (numofrun, remember) => {
+            while(check != numofrun){
+                tempY = tempY-31
+                if(remember === true){
+                    y = tempY
+                }
+                c.fillRect(x,tempY,30,30)
+                check++
+            }
+            tempY = y
+            check = 0
+        }
+
+
+        //block render instructions
+        if (block === "line"){
+            c.fillStyle = "lightblue"
+            c.fillRect(x,y,30,30)
+            if (orientation === "r2" || orientation === "r4"){
+                AddY(1, false)
+                MinusY(2, false)
+            }
+            else if (orientation === "r1" || orientation === "r3") {
+                AddX(2, false)
+                MinusX(1, false)
+            }
+        }
+        else if(block === "box"){
+            c.fillStyle = "green"
+            c.fillRect(x,y,30,30)
+            MinusY(1,true)
+            AddX(1,true)
+            AddY(1, true)
+        }
+        else if(block === "L1"){
+            c.fillStyle = "red"
+            c.fillRect(x,y,30,30)
+            if (orientation === "r1"){
+                AddX(1, false)
+                MinusX(1,true)
+                AddY(1,true)
+
+            }
+            else if (orientation === "r2") {
+                AddY(1,false)
+                MinusY(1,true)
+                MinusX(1,true)
+            }
+            else if (orientation === "r3") {
+                MinusX(1,false)
+                AddX(1,true)
+                MinusY(1,true)
+            }
+            else if (orientation === "r4") {
+                MinusY(1,false)
+                AddY(1,true)
+                AddX(1,true)
+            }
+        }
+        else if(block === "L2"){
+            c.fillStyle = "Purple"
+            c.fillRect(x,y,30,30)
+            if (orientation === "r1"){
+
+            }
+            else if (orientation === "r2") {
+  
+            }
+            else if (orientation === "r3") {
+
+            }
+            else if (orientation === "r4") {
+
+            }  
+        }
+        else if(block === "Z1"){
+            c.fillStyle = "Yellow"
+            c.fillRect(x,y,30,30)
+            if (orientation === "r1"){
+
+            }
+            else if (orientation === "r2") {
+
+            }
+            else if (orientation === "r3") {
+
+            }
+            else if (orientation === "r4") {
+
+            }  
+        }
+        else if(block === "Z2"){
+            c.fillStyle = "Lightgreen"
+            c.fillRect(x,y,30,30)
+            if (orientation === "r1"){
+
+            }
+            else if (orientation === "r2") {
+
+            }
+            else if (orientation === "r3") {
+
+            }
+            else if (orientation === "r4") {
+
+            }  
+        }
+        else if(block === "T"){
+            c.fillStyle = "Orange"
+            c.fillRect(x,y,30,30)
+            if (orientation === "r1"){
+
+            }
+            else if (orientation === "r2") {
+
+            }
+            else if (orientation === "r3") {
+
+            }
+            else if (orientation === "r4") {
+
+            }   
+        }
+
     }
     NextBlock = () => {
         let c = this.canvas.getContext('2d')
         let randomNum = Math.floor(Math.random() * this.blocks.length )
         this.nextblock = this.blocks[randomNum]
         console.log(this.nextblock)
-        let x = 311
-        let y = 32
-        this.Render(this.nextblock, x, y);
+        let x = 373
+        let y = 94
+        this.Render(this.nextblock, x, y, "r4")
 
     }
 }
