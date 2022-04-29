ce74d4a80b90debee5a08e43a58a0ebe061b363f
SpiredFormula <96860840+SpiredFormula@users.noreply.github.com>
2022-04-29 17:32:50 +1000

    movement

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-class Canvas{
-    constructor(nextblock){
-       this.canvas =document.querySelector('canvas')
-       this.rows = {}
-       this.cordsProperties = {}
-       this.cord = []
-       this.blocks = ["line", "box", "L1", "L2", "T", 'Z1', "Z2"]
-       this.nextblock = ''
-       this.currentblock = ''
-       this.BlockPos = []
-       this.NextblockposY = []
+class Canvas {
+    constructor(nextblock) {
+        this.canvas = document.querySelector('canvas')
+        this.rows = {}
+        this.cordsProperties = {}
+        this.cord = []
+        this.blocks = ["line", "box", "L1", "L2", "T", 'Z1', "Z2"]
+        this.nextblock = ''
+        this.currentblock = ''
+        this.BlockPos = []
+        this.NextblockposY = []
     }
     drawCanvas = () => {
         this.canvas.width = 1000//497
@@ -23,31 +23,31 @@ class Canvas{
         let numCollums = 20
         let cords = []
         let rownum = 1
-        while(collums != numCollums){
-            while(rows != numRows){
-                
-                c.fillRect(x,y,30,30)
+        while (collums != numCollums) {
+            while (rows != numRows) {
+
+                c.fillRect(x, y, 30, 30)
                 cords.push(`${x} ${y}`)
                 this.cordsProperties[`${x} ${y}`] = {
                     Color: "gray",
                     isFull: false
                 }
                 this.cord.push(`${x} ${y}`)
-                c.fillStyle  = 'white'
-                c.fillText(`${x},${y}`,x,y+31)
+                //c.fillStyle = 'white'
+                //c.fillText(`${x},${y}`, x, y + 31)
                 c.fillStyle = 'gray'
-                x = x+31
+                x = x + 31
                 rows++
-                
+
             }
             let name = `row${rownum}`
             this.rows[name] = cords
             cords = []
             rownum++
-            y=y+31
-            x=1
+            y = y + 31
+            x = 1
             rows = 0
-            collums++ 
+            collums++
         }
 
 
@@ -55,7 +55,7 @@ class Canvas{
     }
 
 
-    Render = (block, x, y, orientation) =>  {
+    Render = (block, x, y, orientation) => {
         var c = this.canvas.getContext('2d')
         var check = 0
         var tempY = y
@@ -64,65 +64,65 @@ class Canvas{
         let orgY = y
 
         this.BlockPos.push(`${x} ${y}`)
-        this.NextblockposY.push(`${x} ${y+31}`)
-        
+        this.NextblockposY.push(`${x} ${y + 31}`)
+
         //render functions
         let AddY = (numofrun, remember) => {
-            while(check != numofrun){
-                tempY = tempY+31
-                if(remember === true){
+            while (check != numofrun) {
+                tempY = tempY + 31
+                if (remember === true) {
                     y = tempY
                 }
-                c.fillRect(x,tempY,30,30)
+                c.fillRect(x, tempY, 30, 30)
                 check++
                 this.BlockPos.push(`${x} ${tempY}`)
-                this.NextblockposY.push(`${x} ${tempY+31}`)
+                this.NextblockposY.push(`${x} ${tempY + 31}`)
 
-                
+
             }
             tempY = y
             check = 0
         }
         let AddX = (numofrun, remember) => {
-            while(check != numofrun){
-                tempX = tempX+31
-                if(remember === true){
+            while (check != numofrun) {
+                tempX = tempX + 31
+                if (remember === true) {
                     x = tempX
                 }
-                c.fillRect(tempX,y,30,30)
+                c.fillRect(tempX, y, 30, 30)
                 check++
                 this.BlockPos.push(`${tempX} ${y}`)
-                this.NextblockposY.push(`${tempX} ${y+31}`)
-                
+                this.NextblockposY.push(`${tempX} ${y + 31}`)
+
             }
             tempX = x
             check = 0
         }
         let MinusX = (numofrun, remember) => {
-            while(check != numofrun){
-                tempX = tempX-31
-                if(remember === true){
+            while (check != numofrun) {
+                tempX = tempX - 31
+                if (remember === true) {
                     x = tempX
                 }
-                c.fillRect(tempX,y,30,30)
+                c.fillRect(tempX, y, 30, 30)
                 check++
                 this.BlockPos.push(`${tempX} ${y}`)
-                this.NextblockposY.push(`${tempX} ${y+31}`)
+                this.NextblockposY.push(`${tempX} ${y + 31}`)
             }
             tempX = x
             check = 0
         }
         let MinusY = (numofrun, remember) => {
-            while(check != numofrun){
-                tempY = tempY-31
-                if(remember === true){
+            while (check != numofrun) {
+                tempY = tempY - 31
+                if (remember === true) {
                     y = tempY
                 }
-                c.fillRect(x,tempY,30,30)
+                c.fillRect(x, tempY, 30, 30)
                 check++
                 this.BlockPos.push(`${x} ${tempY}`)
-                this.NextblockposY.push(`${x} ${tempY+31}`)
-                
+                this.NextblockposY.push(`${x} ${tempY + 31}`)
+
             }
             tempY = y
             check = 0
@@ -130,11 +130,11 @@ class Canvas{
 
 
         //block render instructions
-        if (block === "line"){
+        if (block === "line") {
             c.fillStyle = "lightblue"
-            c.fillRect(x,y,30,30)
-            
-            if (orientation === "r2" || orientation === "r4"){
+            c.fillRect(x, y, 30, 30)
+
+            if (orientation === "r2" || orientation === "r4") {
                 AddY(1, false)
                 MinusY(2, false)
             }
@@ -143,114 +143,114 @@ class Canvas{
                 MinusX(1, false)
             }
         }
-        else if(block === "box"){
+        else if (block === "box") {
             c.fillStyle = "green"
-            c.fillRect(x,y,30,30)
-            MinusY(1,true)
-            AddX(1,true)
+            c.fillRect(x, y, 30, 30)
+            MinusY(1, true)
+            AddX(1, true)
             AddY(1, true)
         }
-        else if(block === "L1"){
+        else if (block === "L1") {
             c.fillStyle = "red"
-            c.fillRect(x,y,30,30)
-            if (orientation === "r1"){
+            c.fillRect(x, y, 30, 30)
+            if (orientation === "r1") {
                 AddX(1, false)
-                MinusX(1,true)
-                AddY(1,true)
+                MinusX(1, true)
+                AddY(1, true)
 
             }
             else if (orientation === "r2") {
-                AddY(1,false)
-                MinusY(1,true)
-                MinusX(1,true)
+                AddY(1, false)
+                MinusY(1, true)
+                MinusX(1, true)
             }
             else if (orientation === "r3") {
-                MinusX(1,false)
-                AddX(1,true)
-                MinusY(1,true)
-            }
-            else if (orientation === "r4") {
-                MinusY(1,false)
-                AddY(1,true)
-                AddX(1,true)
-            }
-        }
-        else if(block === "L2"){
-            c.fillStyle = "Purple"
-            c.fillRect(x,y,30,30)
-            if (orientation === "r1"){
                 MinusX(1, false)
-                AddX(1,true)
-                AddY(1,true)
+                AddX(1, true)
+                MinusY(1, true)
+            }
+            else if (orientation === "r4") {
+                MinusY(1, false)
+                AddY(1, true)
+                AddX(1, true)
+            }
+        }
+        else if (block === "L2") {
+            c.fillStyle = "Purple"
+            c.fillRect(x, y, 30, 30)
+            if (orientation === "r1") {
+                MinusX(1, false)
+                AddX(1, true)
+                AddY(1, true)
             }
             else if (orientation === "r2") {
-                MinusY(1,false)
-                AddY(1,true)
-                MinusX(1,true)
+                MinusY(1, false)
+                AddY(1, true)
+                MinusX(1, true)
             }
             else if (orientation === "r3") {
-                AddX(1,false)
-                MinusX(1,true)
-                MinusY(1,true)
+                AddX(1, false)
+                MinusX(1, true)
+                MinusY(1, true)
 
             }
             else if (orientation === "r4") {
-                AddY(1,false)
-                MinusY(1,true)
-                AddX(1,true)
-            }  
+                AddY(1, false)
+                MinusY(1, true)
+                AddX(1, true)
+            }
         }
-        else if(block === "Z1"){
+        else if (block === "Z1") {
             c.fillStyle = "Yellow"
-            c.fillRect(x,y,30,30)
-            if (orientation === "r1" || orientation === "r3"){
-                AddX(1,false)
-                AddY(1,true)
-                MinusX(1,true)
-            }
-            else if (orientation === "r2" || orientation === "r4") {
-                AddY(1,false)
-                MinusX(1,true)
-                MinusY(1,true)
-            }
-        }
-        else if(block === "Z2"){
-            c.fillStyle = "Lightgreen"
-            c.fillRect(x,y,30,30)
+            c.fillRect(x, y, 30, 30)
             if (orientation === "r1" || orientation === "r3") {
-                MinusX(1,false)
-                AddY(1,true)
-                AddX(1,true)
+                AddX(1, false)
+                AddY(1, true)
+                MinusX(1, true)
             }
             else if (orientation === "r2" || orientation === "r4") {
-                MinusY(1,false)
-                MinusX(1,true)
-                AddY(1,true)
+                AddY(1, false)
+                MinusX(1, true)
+                MinusY(1, true)
             }
         }
-        else if(block === "T"){
+        else if (block === "Z2") {
+            c.fillStyle = "Lightgreen"
+            c.fillRect(x, y, 30, 30)
+            if (orientation === "r1" || orientation === "r3") {
+                MinusX(1, false)
+                AddY(1, true)
+                AddX(1, true)
+            }
+            else if (orientation === "r2" || orientation === "r4") {
+                MinusY(1, false)
+                MinusX(1, true)
+                AddY(1, true)
+            }
+        }
+        else if (block === "T") {
             c.fillStyle = "Orange"
-            c.fillRect(x,y,30,30)
-            if (orientation === "r1"){
-                AddY(1,false)
-                MinusX(1,false)
-                AddX(1,false)
+            c.fillRect(x, y, 30, 30)
+            if (orientation === "r1") {
+                AddY(1, false)
+                MinusX(1, false)
+                AddX(1, false)
             }
             else if (orientation === "r2") {
-                AddY(1,false)
-                MinusX(1,false)
-                MinusY(1,false)
+                AddY(1, false)
+                MinusX(1, false)
+                MinusY(1, false)
             }
             else if (orientation === "r3") {
-                MinusY(1,false)
-                AddX(1,false)
-                MinusX(1,false)
+                MinusY(1, false)
+                AddX(1, false)
+                MinusX(1, false)
             }
             else if (orientation === "r4") {
-                AddY(1,false)
-                AddX(1,false)
-                MinusY(1,false)
-            }   
+                AddY(1, false)
+                AddX(1, false)
+                MinusY(1, false)
+            }
         }
 
     }
@@ -266,34 +266,34 @@ class Canvas{
         let numCollums = 20
         let cords = []
         let rownum = 1
-        while(collums != numCollums){
-            while(rows != numRows){
-                
-                
-                if (this.cord.includes(`${x} ${y}`)){
-                    c.fillRect(x,y,30,30)
-                    c.fillStyle  = 'white'
-                    c.fillText(`${x},${y}`,x,y+31)
+        while (collums != numCollums) {
+            while (rows != numRows) {
+
+
+                if (this.cord.includes(`${x} ${y}`)) {
+                    c.fillRect(x, y, 30, 30)
+                    //c.fillStyle = 'white'
+                    //c.fillText(`${x},${y}`, x, y + 31)
                     c.fillStyle = 'gray'
                 }
-                else{
+                else {
                     c.fillStyle = "white"
-                    c.fillRect(x,y,30,30)
+                    c.fillRect(x, y, 30, 30)
                     c.fillStyle = 'gray'
                 }
 
-                x = x+31
+                x = x + 31
                 rows++
-                
+
             }
             let name = `row${rownum}`
             this.rows[name] = cords
             cords = []
             rownum++
-            y=y+31
-            x=1
+            y = y + 31
+            x = 1
             rows = 0
-            collums++ 
+            collums++
         }
         this.BlockPos = []
         this.NextblockposY = []
@@ -303,12 +303,12 @@ class Canvas{
 
         let c = this.canvas.getContext('2d')
         c.fillStyle = "blue"
-        c.fillRect(311,1,300,300)
+        c.fillRect(311, 1, 300, 300)
         c.font = "30px Arial"
         c.fillStyle = "white"
         c.fillText("Next block", 400, 30)
         c.font = "10px Arial"
-        let randomNum = Math.floor(Math.random() * this.blocks.length )
+        let randomNum = Math.floor(Math.random() * this.blocks.length)
         this.nextblock = this.blocks[randomNum]
         console.log(this.nextblock)
         let x = 373
@@ -319,14 +319,14 @@ class Canvas{
     CurrentBlock = () => {
         let c = this.canvas.getContext('2d')
         c.fillStyle = "blue"
-        c.fillRect(311,302,300,300)
+        c.fillRect(311, 302, 300, 300)
         c.fillStyle = "white"
         c.font = "30px Arial"
         c.fillText("Current block", 400, 333)
         c.font = "10px Arial"
 
-        let randomNum = Math.floor(Math.random() * this.blocks.length )
-        
+        let randomNum = Math.floor(Math.random() * this.blocks.length)
+
         console.log(this.currentblock)
         let x = 373
         let y = 394
@@ -335,9 +335,9 @@ class Canvas{
 }
 
 const canvas = new Canvas();
-let randomNum = Math.floor(Math.random() * canvas.blocks.length )
+let randomNum = Math.floor(Math.random() * canvas.blocks.length)
 
-canvas.currentblock =  canvas.blocks[randomNum]
+canvas.currentblock = canvas.blocks[randomNum]
 
 canvas.drawCanvas()
 
@@ -345,71 +345,91 @@ canvas.NextBlock()
 canvas.CurrentBlock()
 
 
-class Game{
-    constructor(){
-        this.speed = 400
+class Game {
+    constructor() {
+        this.speed = 200
+
         this.movement = "null"
         this.x = 156
         this.y = 156
-        
+        this.canmove = true
+
     }
 
+    Move = async (direction) => {
+         if (direction === "left") {
+            console.log("left")
+            //canvas.Render(this.currentblock, this.x - 31, this.y, "r2")
+            canvas.RefreshCanvas()
+            canvas.Render(canvas.currentblock, this.x-31, this.y, "r2")
+            this.x = this.x - 31
+            
 
-    Update = async () =>{
+        }
+        if (direction === "right") {
+            canvas.RefreshCanvas()
+            canvas.Render(canvas.currentblock, this.x + 31, this.y, "r2")
+            console.log("right")
+            this.x = this.x + 31
+        }
+
+    }
+
+    Update = async () => {
         let block = canvas.currentblock
         const delay = ms => new Promise(res => setTimeout(res, ms));
         let gameStarted = true
         let x = 156
         let y = 156
         let spawn = false
-        canvas.Render(block, x,y,"r2")
-        while(gameStarted === true){
+        canvas.Render(block, this.x, this.y, "r2")
+        while (gameStarted === true) {
             block = canvas.currentblock
-            await delay (this.speed)
             
+            this.canmove = true
+            
+            await delay(this.speed)
+            this.canmove = false
             canvas.RefreshCanvas()
-            if (spawn = true){
-                
-                
-                canvas.Render(block, x,y, "r2")
-                spawn =false
+            if (spawn = true) {
+
+
+                canvas.Render(block, this.x, this.y, "r2")
+                spawn = false
             }
-            
+
             let checkList = []
             let len = canvas.BlockPos.length
             let i = 0
-            while(i != len){
-                
+            while (i != len) {
+
                 checkList.push(canvas.cord.includes(canvas.NextblockposY[i]))
                 i++
             }
-            
-            if(checkList.includes(false) != true){
 
-                
+            if (checkList.includes(false) != true) {
+
+
                 canvas.RefreshCanvas()
-                canvas.Render(block, x,y,"r2")
-                y= y+31
-                x=x
-                this.movement = "null"
-                this.x = x
-                this.y = y
+                canvas.Render(block, this.x, this.y, "r2")
+                this.y = this.y + 31
+                this.x = this.x
 
                 console.log(`canvas.BlockPos = ${canvas.BlockPos}`)
                 console.log(`Nextblockpos = ${canvas.NextblockposY}`)
-                
+
             }
-            if(checkList.includes(false) === true){
+            if (checkList.includes(false) === true) {
                 console.log("----------------------------------")
-                x = 156
-                y = 156
+                this.x = 156
+                this.y = 156
                 spawn = true
-                
+
                 let z = canvas.BlockPos.length
                 let v = 0
                 let removecords = []
-                
-                while(v != z){
+
+                while (v != z) {
                     let numberofcord = canvas.cord.indexOf(canvas.BlockPos[v])
                     console.log(`numberofcord = ${numberofcord}`)
                     removecords.push(numberofcord)
@@ -426,7 +446,18 @@ class Game{
         }
     }
 
+
 }
 const game = new Game()
+
+
+document.addEventListener("keydown", function (event) {
+    if (event.keyCode === 37 && game.canmove === true) {
+        game.Move("left")
+    }
+    else if (event.keyCode === 39 && game.canmove === true) {
+        game.Move("right")
+    }
+})
 game.Update()
 
